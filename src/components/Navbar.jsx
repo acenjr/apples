4752391c4e34dba992e42e55caad9eb2cd7f73d9
Kelvin Njeru <njrkelvin@gmail.com>
2026-01-02 19:57:54 +0300

    first commit

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+import { navLinks } from "../constants";
+
+const Navbar = () => {
+  return (
+    <header>
+      <nav>
+        <img src="/logo.svg" alt="Apple logo" />
+
+        <ul>
+          {navLinks.map(({ label }) => (
+            <li key={label}>
+              <a href={label}>{label}</a>
+            </li>
+          ))}
+        </ul>
+
+        <div className="flex-center gap-3 ml-auto">
+          <button>
+            <img src="/search.svg" alt="search" />
+          </button>
+          <button>
+            <img src="/cart.svg" alt="search" />
+          </button>
+        </div>
+      </nav>
+    </header>
+  );
+};
+
+export default Navbar;
