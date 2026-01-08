ccd68e45971b3e1e7644b1aacc550b41e3853efc
Kelvin Njeru <njrkelvin@gmail.com>
2026-01-08 16:43:36 +0300

    Footer done

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,29 @@
 import React from "react";
+import { footerLinks } from "../constants";
 
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <footer>
+      <div className="info">
+        <p>
+          More ways to shop: Find an Apple Store or other authorized retailer
+          near you
+        </p>
+        <img src="/logo.svg" alt="Apple logo" />
+      </div>
+      <hr />
+      <div className="links">
+        <p>Copyright 2026. All rights reserved.</p>
+        <ul>
+          {footerLinks.map(({ link, label }) => (
+            <li key={label}>
+              <a href={link}>{label}</a>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
